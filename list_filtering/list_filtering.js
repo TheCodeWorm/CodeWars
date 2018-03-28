b0a064034f4f8746cce6d5efef5b38da74a2e6f4
unknown <4purposeliving@gmail.com>
2018-03-28 12:41:22 -0700

    updated list_filtering using filter instead of for loop

--- list_filtering/list_filtering.js
+++ list_filtering/list_filtering.js
@@ -8,4 +8,21 @@ Example:
 filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
-/*
+
+// using for loop with typeof
+function filter_list(l) {
+	result = [];
+  for (var i=0; i < l.length; i++) {
+    // check if type Number
+  	if (typeof l[i] === 'number') 
+    	result.push(l[i]);
+  }
+  return result;
+}
+*/
+
+// Function: filter_list
+// using filter and typeof
+function filter_list(l) {
+  return l.filter(function(n) {return typeof n == 'number'})
+}
